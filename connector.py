44f0e5ad7bf7ec9e76e49db94db263899a99342c
Runwer <rune.werliin@gmail.com>
2018-08-25 16:15:05 +0200

    Revert "Revert "Connected to AR""
    This reverts commit ad62c8763a006c76ad6468df88acad628cdae151.

---
+++ connector.py
@@ -0,0 +1,31 @@
+import requests
+import json
+
+def getCred(file):
+    with open(file) as f:
+        return json.load(f)
+
+def getToken():
+    baseurl = 'https://oauth.audiencereport.com/oauth/access_token'
+    get_token = getCred('credentials.json')
+    return((requests.post(baseurl, params=get_token)).json())
+
+def getList(token):
+    baseurl = 'https://campaign-api.audiencereport.com/campaigns'
+    header = {'Authorization': 'Bearer '+token}
+    par = {'start': '0', 'maxResults':'1000', 'sortColumn': 'creation-date', 'sortDirection': 'desc' }
+    return(requests.get(baseurl, par, headers=header).json())
+
+def getReport(token, cID):
+    baseurl = 'https://campaign-api.audiencereport.com/reports/'
+    header = {'Authorization': 'Bearer '+token}
+    par = {'id': cID}
+    return(requests.get(baseurl+cID, headers=header).text)
+
+token = getToken()['access_token']
+campaignList = getList(token)
+print(getReport(token, campaignList['data'][1]['id']))
+
+
+
+
