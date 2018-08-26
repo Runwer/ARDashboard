5561bfdeeb0968397af529b5063da8fc7f96a79c
Runwer <rune.werliin@gmail.com>
2018-08-26 18:32:27 +0200

    Prototype done

--- gsupload.py
+++ gsupload.py
@@ -6,40 +6,45 @@ from connector import getCred, getReport, getList, getToken
 
 token = getToken()['access_token']
 campaignList = getList(token)
-parameters = ['name', 'events', 'reach', 'frequency', 'affinity']
+parameters = ['campaignName', 'name', 'events', 'reach', 'frequency', 'affinity']
 
+output = []
+empty = {'campaignName': '', 'name': '', 'events': '', 'reach': '', 'frequency': '', 'affinity': ''}
 
 for c in campaignList['data']:
     data = getReport(token, c['id'])['data']
-    print(c['name'])
-    print('Events: ' + str(data['totals']['events']))
-    print('Reach: ' + str(data['totals']['reach']))
-    print('Frequency: ' + str(data['totals']['frequency']))
-    print('Affinity in target group: ' + str(data['totals']['affinity']))
     for rpi in data['reportItems']:
-        print('CID: ' + str(rpi['name']))
-        print('Events: ' + str(rpi['events']))
-        print('Reach: ' + str(rpi['reach']))
-        print('Frequency: ' + str(rpi['frequency']))
-        print('Affinity in target group: ' + str(rpi['affinity']))
+        outDict = {}
+        outDict['campaignName'] = (c['name'])
+        for p in parameters[1:]:
+            outDict[p] = rpi[p]
+        output.append(outDict)
+    outDict = {}
+    outDict['campaignName'] = (c['name'])
+    outDict['name'] = ('Total')
+    for p in parameters[2:]:
+        outDict[p] = data['totals'][p]
+    output.append(outDict)
+    output.append(empty)
 
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-credentials = ServiceAccountCredentials.from_json_keyfile_name('KPI_Dashboard-9eceeadde073.json', scope)
+credentials = ServiceAccountCredentials.from_json_keyfile_name('KPIDashboard.json', scope)
 gc = gspread.authorize(credentials)
 wks = gc.open('JP-POL-Dashboard').sheet1
 wks.resize(rows=2)
 
-leng = len(campaignList['data'])+1
+leng = len(output)+1
 range1 = wks.range('A2:A'+str(leng))
 i = 0
 for cell in range1:
-    cell.value = dealDictDF[i]['deal_owner']
+    cell.value = output[i]['campaignName']
     i += 1
 for p in range(len(parameters)-1):
     i = 0
     range2 = wks.range(chr(p+98)+'2:'+chr(p+98) + str(leng))
     for cell in range2:
-        cell.value = dealDictDF[i][parameters[p+1]]
+        cell.value = output[i][parameters[p+1]]
+
         i += 1
     range1.extend(range2)
 
